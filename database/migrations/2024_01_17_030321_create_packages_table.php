884a1dbf4ea36e41c5228f029b9c7412df169bc6
Yoss Sagita <68968229+yossasagita121@users.noreply.github.com>
2024-01-17 11:49:37 +0700

    Add files via upload

---
+++ database/migrations/2024_01_17_030321_create_packages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('id_services');
+            $table->string('name');
+            $table->string('description');
+            $table->string('image');
+            $table->string('price');
+            $table->string('time');
+            $table->string('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
---
+++ database/migrations/2024_01_17_030343_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('id_users');
+            $table->string('id_customers');
+            $table->string('id_packages');
+            $table->string('quantity');
+            $table->string('status');
+            $table->string('total_price');
+            $table->string('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
